8d1f56b0955aba3d0a519917076b35b66e9ca7cf
mmoulou <moulou.mouhcine@gmail.com>
2012-11-06 15:33:07 +0100

    add pagination method

--- cassandra-mock/src/main/java/fr/soat/cassandra/astyanax/ProductNoSqlRepository.java
+++ cassandra-mock/src/main/java/fr/soat/cassandra/astyanax/ProductNoSqlRepository.java
@@ -3,14 +3,21 @@ package fr.soat.cassandra.astyanax;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
 import com.netflix.astyanax.MutationBatch;
 import com.netflix.astyanax.connectionpool.OperationResult;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 import com.netflix.astyanax.model.ColumnFamily;
 import com.netflix.astyanax.model.ColumnList;
+import com.netflix.astyanax.model.Row;
+import com.netflix.astyanax.model.Rows;
+import com.netflix.astyanax.query.IndexQuery;
 import com.netflix.astyanax.serializers.StringSerializer;
 
 import fr.soat.bean.Product;
@@ -99,6 +106,33 @@ public class ProductNoSqlRepository extends AbstractNoSqlRepository{
 		} catch (ConnectionException e) {
 		      logger.error("Unexpected Exception while trying to delete row with product ref: " + ref + ". -" + e);
 		}
+	}
+	
+	/**
+	 * @param StartFrom
+	 * @param maxResut
+	 * @return
+	 */
+	public List<Product> getProducts(String StartFrom, int maxResut){
+
+		List<Product> products = Lists.newArrayList();
+		try {
+			IndexQuery<String, String> query = getKeyspace()
+			   .prepareQuery(ProductColumnFamily).searchWithIndex().setStartKey(StartFrom)
+			   .setRowLimit(maxResut).autoPaginateRows(true);
+			
+			Rows<String, String> result = query.execute().getResult();
+			for (Iterator<Row<String, String>> iterator = result.iterator(); iterator.hasNext();) {
+				Row<String, String> row = iterator.next();
+				products.add(createProduct(row.getKey(), row.getColumns()));
+				
+			}
+			
+		} catch (Exception e) {
+			logger.error("" + e);
+		}
+		
+		return products;
 	}
 
 	/**
